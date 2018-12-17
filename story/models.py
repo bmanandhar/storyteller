269ff4e869dc776f0c9157f3a8a0b68078c8eb22
jbortiz0623 <jbortiz0623@gmail.com>
2018-12-17 09:55:24 -0800

    created templates

--- story/models.py
+++ story/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+ 
 # Create your models here.
+class Story(models.Model):
+    user = models.CharField(max_length=200)
+    body = models.TextField()
+    date = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
