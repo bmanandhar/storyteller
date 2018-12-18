d655cedd5ce36779cf6c07944a2e2a1f87ba943b
jbortiz0623 <jbortiz0623@gmail.com>
2018-12-17 17:23:27 -0800

    intialized CRUD functionality

--- story/urls.py
+++ story/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
-    
+urlpatterns=  [
+    # url(r'^$', views.index),
+    path('create', views.create),
+    # url(r'edit/(?P<id>\d+)$', views.edit),
+    # url(r'update/(?P<id>\d+)$', views.update),
+    # url(r'delete/(?P<id>\d+)$', views.destroy)
+
 ]
--- story/views.py
+++ story/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Story
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
+
+def index(request):
+    storys = Story.objects.all()
+    context = {'storys':storys}
+    return render(request, 'story/index.html', context)
+@csrf_exempt
+def create(request):
+    # print request.POST
+    print('vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv')
+    story = Story(
+    user=request.POST['user'], 
+    body=request.POST['body'],
+    date=request.POST['date'],
+    title=request.POST['title'],)
+    stry =  story.save()
+    print(stry)
+    return JsonResponse({'foo':'bar'})
+
+
+def edit(request, id):
+    story = Story.objects.get(id=id)
+    context = {'story':story}
+    return render(request, 'story/edit.html', context )
+
+def update(request, id):
+    story = Story.objects.get(id=id)
+    story.user = request.POST['user']
+    story.body = request.POST['body']
+    story.date = request.POST['date']
+    stroy.title = request.POST['title']
+    story.save()
+    return redirect('/')
+
+def delete(request, id):
+    story = Story.objects.delete(id=id)
+    story.delete()
+    return redirect('/')
